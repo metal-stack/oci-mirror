4501081a31d54adf248ad90da41c40cc06a8c13c
Stefan Majer <stefan.majer@f-i-ts.de>
2023-12-05 15:54:06 +0100

    Log duration

--- cmd/server.go
+++ cmd/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log/slog"
+	"time"
 
 	apiv1 "github.com/metal-stack/oci-mirror/api/v1"
 	"github.com/metal-stack/oci-mirror/pkg/mirror"
@@ -21,11 +22,13 @@ func newServer(log *slog.Logger, config apiv1.Config) *server {
 }
 
 func (s *server) run() error {
+	start := time.Now()
 	m := mirror.New(s.log, s.config)
 	err := m.Mirror(context.Background())
 	if err != nil {
 		s.log.Error("error mirroring images", "error", err)
 		return err
 	}
+	s.log.Info("finished mirroring after", "duration", time.Since(start))
 	return nil
 }
